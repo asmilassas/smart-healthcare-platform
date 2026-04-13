59d8aafe00b9f60ed2420b0d208b619b69e65626
IT23136410 <IT23136410@my.sliit.lk>
2026-04-13 20:59:18 +0530

    Appointment and Doctor Service

---
+++ services/doctor-management-service/src/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const doctorRoutes = require("./routes/doctorRoutes");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Doctor Management Service is running");
+});
+
+app.use("/api/doctors", doctorRoutes);
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal server error" });
+});
+
+const PORT = process.env.PORT || 5002;
+
+app.listen(PORT, () => {
+  console.log(`Doctor Management Service running on port ${PORT}`);
+});
